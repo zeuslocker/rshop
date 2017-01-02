2e7af7b09ceddc4582115819a5d012d7a8f749df
zeuslocker <lapan40@ukr.net>
2017-01-02 16:18:00 +0200

    w1 checkout work well, but need to improve

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,23 @@
+require "net/http"
+require "uri"
 class OrdersController < ApplicationController
   WALLETONE_SECRET_KEY='Ваш секретный ключ от Walletone'
 
 def create
-#    render json: params
   @payment = Walletone::Payment.new(
     WMI_MERCHANT_ID:    '147573326814',
-    WMI_PAYMENT_AMOUNT:  params[:price], # Сумма
-    WMI_CURRENCY_ID:     980, # ISO номер валюты (По умолчанию 643 - Рубль),
-    WMI_PTENABLED:      ['WebMoneyRUB', 'WebMoneyUSD'], # Указывать не обязательно
+    WMI_PAYMENT_AMOUNT:  params[:price],
+    WMI_CURRENCY_ID:     980, # ISO номер валюти 980 - гривня
+    WMI_DESCRIPTION: 'Оплата заказа iStore!',
+    WMI_CULTURE_ID: 'ru-RU',
     SOME_CUSTOM_FIELD:  'value'
-    # etc любые другие поля
   )
   @payment.sign! WALLETONE_SECRET_KEY
-
- render partial: 'form', locals: { payment: @payment }
+#  uri = URI.parse('https://wl.walletone.com/checkout/checkout/Index')
+#  response = Net::HTTP.post_form(uri, @payment.to_h)
+#  p 'hjgjhgffffffjhjj'+@payment.WMI_AUTO_LOCATION
+# payment_uri = 'https://wl.walletone.com'+response.to_hash.fetch('location')[0]
+# redirect_to payment_uri
+render partial: 'form', locals: {payment: @payment}
 end
 end
